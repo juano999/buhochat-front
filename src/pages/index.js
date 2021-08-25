f743c1525fad31109c8f62e867a666c6364491b0
Juan Bolaños <carl-bo@hotmail.com>
2021-08-24 19:58:14 -0500

    cambios en _app y _document, se agrega theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,113 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import styles from '../styles/Home.module.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+
+
+  },
+  Box: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    backgroundColor: 'red',
+    height: '200px',
+
+  },
+  divider: {
+    backgroundColor: '#052E5E',
+    height: '2px',
+  },
+  grid: {
+    marginBottom: '20px',
+    marginTop: '20px',
+  },
+}));
 
 export default function Home() {
+
+
+
+  const classes = useStyles();
   return (
-    <div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
 
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={6}>
+            <Box className={classes.Box}>slider</Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box className={classes.Box}>login</Box>
+          </Grid>
+        </Grid>
 
-    </div>
+        <Divider className={classes.divider} />
+
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=12</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=12</Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=12</Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=12</Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box className={classes.Box}>xs=6</Box>
+          </Grid>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+      </Container>
+    </React.Fragment>
   )
 }
