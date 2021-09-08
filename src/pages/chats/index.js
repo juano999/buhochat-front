059590cbf23154cec32bdfbecfaaa86bf5b8fc26
Jorge Alba <jorge.alba@epn.edu.ec>
2021-09-08 17:33:58 -0500

    Diseño v1

---
+++ src/pages/chats/index.js
@@ -0,0 +1,18 @@
+import HeaderChat from './headerChat.js';
+import Footer from './footerChat.js'
+import ChatBody from './ChatBody.js'
+
+const ChatPage = () => {
+
+    
+
+    return (
+        <div>
+            <HeaderChat></HeaderChat>
+            <ChatBody></ChatBody>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default ChatPage;
